9ed1d1c630062cc8cd19e9695b97d6955ef4a146
Rafael Gomes <rafags2005@hotmail.com>
2020-07-28 20:43:59 -0300

    Ajustando seções

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,14 +4,11 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
       <p>
         Orgulhosamente criado durante a
         {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+        <a href="https://pt.wikipedia.org/wiki/Pandemia_de_COVID-19">
+          quarentena
         </a>
       </p>
     </FooterBase>
